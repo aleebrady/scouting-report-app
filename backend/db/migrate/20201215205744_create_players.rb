eca49671d68caea22b1697ee3217abb3129cde68
aleebrady <aleebrady@gmail.com>
2020-12-15 13:00:45 -0800

    db setup with models

---
+++ backend/db/migrate/20201215205744_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.integer :number
+      t.string :position
+      t.string :team
+      t.integer :team_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ backend/db/migrate/20201215205930_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teams do |t|
+      t.string :city
+      t.string :name
+      t.string :coach
+      t.integer :est
+
+      t.timestamps
+    end
+  end
+end
